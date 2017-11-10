58fad972a03dfcc52627818cf31cb85121050e31
Adrian Perez de Castro <aperez@igalia.com>
2017-11-10 15:55:20 +0200

    Use GtkApplicationWindow to automatically holds/release the app
    Using GtkApplicationWindow (instead of GtkWindow) will automatically
    call g_application_hold() when the window is added, and conversely
    g_application_release() after the window is destroyed.

--- dy-gtk-utils.c
+++ dy-gtk-utils.c
@@ -8,15 +8,6 @@
 #include "dy-gtk-utils.h"
 
 
-static void
-on_window_destroy (GtkWidget    *window,
-                   GApplication *application)
-{
-    g_printerr ("%s: window=%p, launcher=%p\n", __func__, window, application);
-    g_application_release (application);
-}
-
-
 GtkWidget*
 dy_gtk_create_window (DyLauncher *launcher)
 {
@@ -30,12 +21,10 @@ dy_gtk_create_window (DyLauncher *launcher)
     gtk_header_bar_set_title (GTK_HEADER_BAR (header), "Dinghy");
     g_object_bind_property (web_view, "uri", header, "subtitle", G_BINDING_DEFAULT);
 
-    GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);  // Floating.
+    GtkWidget *window = gtk_application_window_new (GTK_APPLICATION (launcher));  // Floating.
     gtk_window_set_titlebar (GTK_WINDOW (window), header);  // Takes ownwership of header.
     gtk_widget_set_size_request (window, 400, 200);
     gtk_container_add (GTK_CONTAINER (window), web_view);
-    g_signal_connect (window, "destroy", G_CALLBACK (on_window_destroy), launcher);
-    g_application_hold (G_APPLICATION (launcher));
 
     gtk_widget_show_all (window);
     return window;
