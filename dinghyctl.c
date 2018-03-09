f2eb1c93d29bfb2dc2d900433053febf4a265d27
Adrian Perez de Castro <aperez@igalia.com>
2018-03-09 00:28:44 +0000

    dinghyctl: Rewrite tool in C
    This allows using "dinghyctl" in systems where the "busctl" tool is not
    available, without introducing dependencies on any other similar tool
    (dbus-send, gdbus, etc). This may come in particularly handy in embedded
    scenarios.

---
+++ dinghyctl.c
@@ -0,0 +1,348 @@
+/*
+ * dinghyctl.c
+ * Copyright (C) 2018 Adrian Perez <aperez@igalia.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "dy-config.h"
+
+#include <gio/gio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef DY_DEFAULT_APPID
+#  if DY_USE_WEBKITGTK
+#    define DY_DEFAULT_APPID "com.igalia.DinghyGtk"
+#  else
+#    define DY_DEFAULT_APPID "com.igalia.Dinghy"
+#  endif
+#endif
+
+#define IF_FDO_APPLICATION "org.freedesktop.Application"
+#define IF_FDO_DBUS_PEER   "org.freedesktop.DBus.Peer"
+
+#define FDO_APPLICATION_ACTIVATE_ACTION \
+    IF_FDO_APPLICATION, "ActivateAction"
+#define FDO_APPLICATION_OPEN \
+    IF_FDO_APPLICATION, "Open"
+#define FDO_DBUS_PEER_PING \
+    IF_FDO_DBUS_PEER, "Ping"
+
+
+static struct {
+    char    *appid;
+    gboolean print_appid;
+} s_options = {
+    .appid = DY_DEFAULT_APPID,
+};
+
+
+static GOptionEntry s_cli_options[] = {
+    { "appid", 'A', 0, G_OPTION_ARG_STRING, &s_options.appid,
+        "Application identifier of the Dinghy instance to control",
+        "ID" },
+    { NULL, }
+};
+
+
+static char*
+appid_to_object_path (const char *appid)
+{
+    GString *s = g_string_new ("/");
+    for (; *appid; appid++) {
+        g_string_append_c (s, (*appid == '.') ? '/' : *appid);
+    }
+    return g_string_free (s, FALSE);
+}
+
+
+static gboolean
+call_method (const char *iface,
+             const char *method,
+             GVariant   *params,
+             GError    **error)
+{
+    g_autoptr(GDBusConnection) conn =
+        g_bus_get_sync (G_BUS_TYPE_SESSION, NULL, error);
+    if (!error)
+        return FALSE;
+
+    g_autofree char *object_path =
+        appid_to_object_path (s_options.appid);
+    g_autoptr(GVariant) result =
+        g_dbus_connection_call_sync (conn,
+                                     s_options.appid,
+                                     object_path,
+                                     iface,
+                                     method,
+                                     params,
+                                     NULL,
+                                     G_DBUS_CALL_FLAGS_NO_AUTO_START,
+                                     -1,
+                                     NULL,
+                                     error);
+    return !!result;
+}
+
+
+struct cmd {
+    const char *name;
+    const char *desc;
+    int (*handler) (const char *name, const void *data, int argc, char **argv);
+    const void *data;
+};
+
+static const struct cmd* cmd_find_by_name (const char *name);
+
+
+static void
+cmd_check_simple_help (const char *name, int needed_argc, int *argc, char ***argv)
+{
+    const char *space = strchr (name, ' ');
+    if (!space) space = strchr (name, '\0');
+
+    g_autofree char *cmd_name = g_strndup (name, space - name);
+
+    g_autoptr(GOptionContext) option_context = g_option_context_new (name);
+    g_option_context_set_description (option_context,
+                                      cmd_find_by_name (cmd_name)->desc);
+    g_option_context_add_main_entries (option_context,
+                                       &((GOptionEntry) { NULL, }),
+                                       NULL);
+
+    g_autoptr(GError) error = NULL;
+    if (!g_option_context_parse (option_context, argc, argv, &error) || *argc > (1 + needed_argc)) {
+        g_printerr ("%s: %s\n", name, error ? error->message : "No arguments expected");
+        exit (EXIT_FAILURE);
+    }
+}
+
+
+static int
+cmd_generic_alias (const char *name,
+                   const void *data,
+                   int         argc,
+                   char      **argv)
+{
+    const struct cmd *cmd = cmd_find_by_name ((const char*) data);
+    return (*cmd->handler) (cmd->name, cmd->data, argc, argv);
+}
+
+static int
+cmd_generic_no_args (const char               *name,
+                     G_GNUC_UNUSED const void *data,
+                     int                       argc,
+                     char                    **argv)
+{
+    cmd_check_simple_help (name, 0, &argc, &argv);
+
+    GVariant *params = g_variant_new ("(sava{sv})", name, NULL, NULL);
+
+    g_autoptr(GError) error = NULL;
+    if (!call_method (FDO_APPLICATION_ACTIVATE_ACTION, params, &error)) {
+        g_printerr ("%s\n", error->message);
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+static int
+cmd_appid (const char               *name,
+           G_GNUC_UNUSED const void *data,
+           int                       argc,
+           char                    **argv)
+{
+    cmd_check_simple_help (name, 0, &argc, &argv);
+    g_print ("%s\n", s_options.appid);
+    return EXIT_SUCCESS;
+}
+
+
+static int
+cmd_open (const char               *name,
+          G_GNUC_UNUSED const void *data,
+          int                       argc,
+          char                    **argv)
+{
+    cmd_check_simple_help ("open URL", 1, &argc, &argv);
+
+    g_autoptr(GVariantBuilder) uris =
+        g_variant_builder_new (G_VARIANT_TYPE ("as"));
+    g_variant_builder_add (uris, "s", argv[1]);
+
+    GVariant *params = g_variant_new ("(asa{sv})", uris, NULL);
+
+    g_autoptr(GError) error = NULL;
+    if (!call_method (FDO_APPLICATION_OPEN, params, &error)) {
+        g_printerr ("%s\n", error->message);
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+
+static int
+cmd_ping (const char               *name,
+          G_GNUC_UNUSED const void *data,
+          int                       argc,
+          char                    **argv)
+{
+    cmd_check_simple_help (name, 0, &argc, &argv);
+    return call_method (FDO_DBUS_PEER_PING, NULL, NULL)
+        ? EXIT_SUCCESS
+        : EXIT_FAILURE;
+}
+
+
+static int
+cmd_help (const char *name,
+          const void *data,
+          int         argc,
+          char      **argv)
+{
+    g_autoptr(GOptionContext) option_context =
+        g_option_context_new ("help [command]");
+    g_option_context_add_main_entries (option_context,
+                                       &((GOptionEntry) { NULL, }),
+                                       NULL);
+
+    g_autoptr(GError) error = NULL;
+    if (!g_option_context_parse (option_context, &argc, &argv, &error)) {
+        g_printerr ("%s: %s\n", argv[0], error->message);
+        return EXIT_FAILURE;
+    }
+
+    if (argc == 1) {
+        g_print ("Available commands:\n");
+        const struct cmd *cmds = data;
+        for (unsigned i; cmds[i].name; i++) {
+            if (cmds[i].desc) {
+                g_print ("  %-10s %s\n", cmds[i].name, cmds[i].desc);
+            }
+        }
+    } else if (argc == 2) {
+        const struct cmd *cmd = cmd_find_by_name (argv[1]);
+        if (!cmd) {
+            g_printerr ("Unrecognized command: %s\n", argv[1]);
+            return EXIT_FAILURE;
+        }
+
+        if (cmd->handler == cmd_generic_alias) {
+            g_print ("'%s' is aliased to '%s'\n", argv[1], cmd->data);
+        } else {
+            char *cmd_argv[] = { (char*) cmd->name, "--help" };
+            return (*cmd->handler) (cmd->name, cmd->data, G_N_ELEMENTS (cmd_argv), cmd_argv);
+        }
+    } else {
+        g_printerr ("Invalid command line ussage\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+static const struct cmd*
+cmd_find_by_name (const char *name)
+{
+    static const struct cmd cmdlist[] = {
+        {
+            .name = "appid",
+            .desc = "Display application ID being remotely controlled",
+            .handler = cmd_appid,
+        },
+        {
+            .name = "help",
+            .desc = "Obtain help about commands",
+            .data = cmdlist,
+            .handler = cmd_help,
+        },
+        {
+            .name = "open",
+            .desc = "Open an URL",
+            .handler = cmd_open,
+        },
+        {
+            .name = "previous",
+            .desc = "Navigate backward in the page view history",
+            .handler = cmd_generic_no_args,
+        },
+        {
+            .name = "prev",
+            .data = "previous",
+            .handler = cmd_generic_alias,
+        },
+        {
+            .name = "next",
+            .desc = "Navigate forward in the page view history",
+            .handler = cmd_generic_no_args,
+        },
+        {
+            .name = "ping",
+            .desc = "Check whether Dinghy is running",
+            .handler = cmd_ping,
+        },
+        {
+            .name = "quit",
+            .desc = "Exit the application",
+            .handler = cmd_generic_no_args,
+        },
+        {
+            .name = "reload",
+            .desc = "Reload the current page",
+            .handler = cmd_generic_no_args,
+        },
+        { NULL },
+    };
+
+    for (unsigned i = 0; i < G_N_ELEMENTS (cmdlist); i++)
+        if (strcmp (cmdlist[i].name, name) == 0)
+            return &cmdlist[i];
+
+    return NULL;
+}
+
+
+static const char s_extended_help_text[] =
+    "For a list of commands use:\n"
+    "  dinghyctl help\n"
+    "\n"
+    "Help on each command can be obtained with one of:\n"
+    "  dinghyctl help <command>\n"
+    "  dinghyctl <command> --help\n";
+
+
+int
+main (int argc, char **argv)
+{
+    g_autoptr(GOptionContext) option_context =
+        g_option_context_new ("<subcommand> [SUBCOMMAND-OPTION?]");
+    g_option_context_set_description (option_context, s_extended_help_text);
+    g_option_context_add_main_entries (option_context, s_cli_options, NULL);
+    g_option_context_set_strict_posix (option_context, TRUE);
+
+    g_autoptr(GError) error = NULL;
+    if (!g_option_context_parse (option_context, &argc, &argv, &error)) {
+        g_printerr ("Command line error: %s\n", error->message);
+        return EXIT_FAILURE;
+    }
+
+    if (!g_application_id_is_valid (s_options.appid)) {
+        g_printerr ("Invalid application ID: %s\n", s_options.appid);
+        return EXIT_FAILURE;
+    }
+
+    if (argc < 2) {
+        g_printerr ("Missing subcommand\n");
+        return EXIT_FAILURE;
+    }
+
+    const struct cmd *cmd = cmd_find_by_name (argv[1]);
+    if (!cmd) {
+        g_printerr ("Unrecognized command: %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    return (*cmd->handler) (cmd->name, cmd->data, argc - 1, argv + 1);
+}
