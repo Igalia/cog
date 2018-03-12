b6d621af5d444fa3a2b78cc6da41463c33a78cc2
Adrian Perez de Castro <aperez@igalia.com>
2018-03-12 14:18:50 +0000

    Fix handling of custom URI scheme handling in CLI arguments
    For any URI that did not have either http:// or https:// or wasn't
    a file the http:// prefix was being added inconditionally, which of
    course breaks custom URI schemes. Instead of checking against a fixed
    set of schemes, properly parse the URI and use the SoupURI APIs to
    check and modify the URI fields, and re-construct the final URI which
    gets passed to WebKit for loading.
    
    Note that does not really add any dependency, as libsoup is needed
    already by WebKitWPE (and WebKitGTK+).
    
    Fixes #5

--- dy-utils.c
+++ dy-utils.c
@@ -6,6 +6,7 @@
  */
 
 #include "dy-utils.h"
+#include <libsoup/soup.h>
 #include <gio/gio.h>
 #include <string.h>
 
@@ -17,13 +18,6 @@ dy_uri_guess_from_user_input (const char *uri_like,
 {
     g_return_val_if_fail (uri_like, NULL);
 
-    g_autoptr(GFile) file = is_cli_arg
-        ? g_file_new_for_commandline_arg (uri_like)
-        : g_file_new_for_path (uri_like);
-
-    if (g_file_is_native (file) && g_file_query_exists (file, NULL))
-        return g_file_get_uri (file);
-
     g_autofree char *utf8_uri_like = NULL;
     if (is_cli_arg) {
         if (!(utf8_uri_like = g_locale_to_utf8 (uri_like, -1, NULL, NULL, error)))
@@ -32,9 +26,37 @@ dy_uri_guess_from_user_input (const char *uri_like,
         utf8_uri_like = g_strdup (uri_like);
     }
 
-    g_autofree char *lc = g_utf8_strdown (utf8_uri_like, -1);
-    if (g_str_has_prefix (lc, "http://") || g_str_has_prefix (lc, "https://"))
-        return g_steal_pointer (&utf8_uri_like);
+    g_autoptr(SoupURI) uri = soup_uri_new (utf8_uri_like);
+    if (uri) {
+        // GFile does not handle query strings nor fragments, so use it only
+        // to get obtain the absolute path and modify the SoupURI in place.
+        if (uri->scheme == SOUP_URI_SCHEME_FILE && uri->path && *uri->path != '\0') {
+            g_autoptr(GFile) file = g_file_new_for_path (uri->path);
+            g_autofree char *path = g_file_get_path (file);
+            if (path) soup_uri_set_path (uri, path);
+        }
+
+        // If we have a scheme, do not try to guess whether the URI is a local
+        // file or whether a scheme should be added to it, just return it. This
+        // also covers the case of custom URI scheme handlers.
+        if (uri->scheme) {
+            // Allow "scheme:" to be a shorthand for "scheme:/", which is handy
+            // when using custom URI scheme handlers.
+            if (!uri->path || *uri->path == '\0')
+                soup_uri_set_path (uri, "/");
+            return soup_uri_to_string (uri, FALSE);
+        }
+    }
+
+    // At this point we know that we have been given a shorthand without an
+    // URI scheme, or something that cannot be parsed as an URI: try to find
+    // a local file, otherwise add http:// as the scheme.
+    g_autoptr(GFile) file = is_cli_arg
+        ? g_file_new_for_commandline_arg (uri_like)
+        : g_file_new_for_path (uri_like);
+
+    if (g_file_is_native (file) && g_file_query_exists (file, NULL))
+        return g_file_get_uri (file);
 
     return g_strconcat ("http://", utf8_uri_like, NULL);
 }
