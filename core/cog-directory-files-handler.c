Igalia/cog

commit f1b55aba0a00d1751a4f5f0f261c9db314bfc36f

core: Ensure that files URI handler does not escape the base path
Return early from cog_directory_files_handler_run() if the calculated
path to the location of the resource on disk happens to not be a
descendant of the base path.

core/cog-directory-files-handler.c
@@ -187,6 +187,24 @@ cog_directory_files_handler_run (CogRequestHandler      *request_handler,
         ++path;
 
     g_autoptr(GFile) file = g_file_get_child (base_path, path);
+
+    /*
+     * Check whether the resolved path is contained inside base_path,
+     * to prevent URIs with ".." components from accessing resources
+     * outside of base_path. The g_file_get_relative_path() method
+     * returns NULL when the file path is NOT descendant of base_path.
+     */
+    g_autofree char *relative_path = g_file_get_relative_path (base_path, file);
+    if (!relative_path) {
+        g_autoptr(GError) error = g_error_new (G_FILE_ERROR,
+                                               G_FILE_ERROR_PERM,
+                                               "Resolved path '%s' not "
+                                               "contained in base path '%s'",
+                                               g_file_peek_path (file),
+                                               g_file_peek_path (base_path));
+        return webkit_uri_scheme_request_finish_error (request, error);
+    }
+
     g_file_query_info_async (file,
                              s_file_query_attributes,
                              G_FILE_QUERY_INFO_NONE,

commit cc96b58002648d0a6d7e0bf71908f61c7d0e6616

core: Allow resolving base path in URI files handler
When checking whether a resolved path is a descendant of the base path
in cog_directory_files_handler_run(), allow the resolved path to be the
same as the base path. This is needed to allow URIs which point to the
root of the base path (either without a path, or a single slash as path)
and attempt to find an "index.html" file there.

This fixes a regression introduced by #265

core/cog-directory-files-handler.c
@@ -192,17 +192,20 @@ cog_directory_files_handler_run (CogRequestHandler      *request_handler,
      * Check whether the resolved path is contained inside base_path,
      * to prevent URIs with ".." components from accessing resources
      * outside of base_path. The g_file_get_relative_path() method
-     * returns NULL when the file path is NOT descendant of base_path.
+     * returns NULL when the file path is NOT descendant of base_path,
+     * or when both paths are the same.
      */
-    g_autofree char *relative_path = g_file_get_relative_path (base_path, file);
-    if (!relative_path) {
-        g_autoptr(GError) error = g_error_new (G_FILE_ERROR,
-                                               G_FILE_ERROR_PERM,
-                                               "Resolved path '%s' not "
-                                               "contained in base path '%s'",
-                                               g_file_peek_path (file),
-                                               g_file_peek_path (base_path));
-        return webkit_uri_scheme_request_finish_error (request, error);
+    if (!g_file_equal (base_path, file)) {
+        g_autofree char *relative_path = g_file_get_relative_path (base_path, file);
+        if (!relative_path) {
+            g_autoptr(GError) error = g_error_new (G_FILE_ERROR,
+                                                   G_FILE_ERROR_PERM,
+                                                   "Resolved path '%s' not "
+                                                   "contained in base path '%s'",
+                                                   g_file_peek_path (file),
+                                                   g_file_peek_path (base_path));
+            return webkit_uri_scheme_request_finish_error (request, error);
+        }
     }
 
     g_file_query_info_async (file,