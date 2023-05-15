d640d404d20d497043f030702322accad2d63675
Adrian Perez de Castro <aperez@igalia.com>
2023-05-16 00:40:09 +0300

    headless: Support multiple views
    Support more than one view. This is achieved by opting-in to use the new
    CogView.create_backend vfunc, which makes each web view responsible for
    creating its own WPE view backend. The platform object keeps a reference
    to the shell's CogViewGroup, and on each frame timer tick all views in
    the group are checked for dispatching frame acknowledgements.

--- platform/headless/cog-platform-headless.c
+++ platform/headless/cog-platform-headless.c
@@ -1,8 +1,8 @@
 /*
  * cog-platform-headless.c
- * Copyright (C) 2021 Igalia S.L
+ * Copyright (C) 2021, 2023 Igalia S.L
  *
- * Distributed under terms of the MIT license.
+ * SPDX-License-Identifier: MIT
  */
 
 #include "../../core/cog.h"
@@ -11,10 +11,15 @@
 #include <wpe/fdo.h>
 #include <wpe/unstable/fdo-shm.h>
 
-typedef struct {
+struct _CogHeadlessView {
+    CogView parent;
+
     bool                                    frame_ack_pending;
     struct wpe_view_backend_exportable_fdo *exportable;
-} CogHeadlessView;
+};
+
+G_DECLARE_FINAL_TYPE(CogHeadlessView, cog_headless_view, COG, HEADLESS_VIEW, CogView)
+G_DEFINE_DYNAMIC_TYPE(CogHeadlessView, cog_headless_view, COG_TYPE_VIEW)
 
 struct _CogHeadlessPlatformClass {
     CogPlatformClass parent_class;
@@ -26,8 +31,7 @@ struct _CogHeadlessPlatform {
     unsigned max_fps;
     unsigned tick_source;
 
-    /* TODO: Move elsewhere once multiple views are supported */
-    CogHeadlessView view;
+    CogViewGroup *views;
 };
 
 G_DECLARE_FINAL_TYPE(CogHeadlessPlatform, cog_headless_platform, COG, HEADLESS_PLATFORM, CogPlatform)
@@ -46,25 +50,59 @@ static void on_export_shm_buffer(void* data, struct wpe_fdo_shm_exported_buffer*
     view->frame_ack_pending = true;
 }
 
-static gboolean
-on_cog_headless_platform_tick(CogHeadlessPlatform *self)
+static void
+on_cog_headless_view_backend_destroy(CogHeadlessView *self)
 {
-    CogHeadlessView *view = &self->view;
-    if (view->frame_ack_pending) {
-        view->frame_ack_pending = false;
-        wpe_view_backend_exportable_fdo_dispatch_frame_complete(view->exportable);
-    }
-    return G_SOURCE_CONTINUE;
+    g_debug("%s: view %p, exportable %p", G_STRFUNC, self, self->exportable);
+    g_assert(self->exportable);
+    g_clear_pointer(&self->exportable, wpe_view_backend_exportable_fdo_destroy);
 }
 
-static void
-cog_headless_view_initialize(CogHeadlessView *self, CogHeadlessPlatform *platform)
+static WebKitWebViewBackend *
+cog_headless_view_create_backend(CogView *view)
 {
+    CogHeadlessView *self = COG_HEADLESS_VIEW(view);
+
     static const struct wpe_view_backend_exportable_fdo_client client = {
         .export_shm_buffer = on_export_shm_buffer,
     };
 
     self->exportable = wpe_view_backend_exportable_fdo_create(&client, self, 800, 600);
+
+    struct wpe_view_backend *view_backend = wpe_view_backend_exportable_fdo_get_view_backend(self->exportable);
+    return webkit_web_view_backend_new(view_backend, (GDestroyNotify) on_cog_headless_view_backend_destroy, self);
+}
+
+static void
+cog_headless_view_class_init(CogHeadlessViewClass *klass)
+{
+    CogViewClass *view_class = COG_VIEW_CLASS(klass);
+    view_class->create_backend = cog_headless_view_create_backend;
+}
+
+static void
+cog_headless_view_class_finalize(CogHeadlessViewClass *klass G_GNUC_UNUSED)
+{
+}
+
+static void
+cog_headless_view_init(CogHeadlessView *self G_GNUC_UNUSED)
+{
+}
+
+static gboolean
+on_cog_headless_platform_tick(CogHeadlessPlatform *self)
+{
+    g_assert(self->views);
+
+    for (unsigned i = 0; i < cog_view_group_get_n_views(self->views); i++) {
+        CogHeadlessView *view = COG_HEADLESS_VIEW(cog_view_group_get_nth_view(self->views, i));
+        if (view->frame_ack_pending) {
+            view->frame_ack_pending = false;
+            wpe_view_backend_exportable_fdo_dispatch_frame_complete(view->exportable);
+        }
+    }
+    return G_SOURCE_CONTINUE;
 }
 
 static gboolean
@@ -84,8 +122,8 @@ cog_headless_platform_setup(CogPlatform* platform, CogShell* shell G_GNUC_UNUSED
     }
     g_debug("Maximum refresh rate: %u FPS", self->max_fps);
 
-    cog_headless_view_initialize(&self->view, self);
     self->tick_source = g_timeout_add(1000.0 / self->max_fps, G_SOURCE_FUNC(on_cog_headless_platform_tick), self);
+    self->views = COG_VIEW_GROUP(cog_shell_get_view_stack(shell)); /* XXX: Ref? */
     return TRUE;
 }
 
@@ -100,26 +138,6 @@ cog_headless_platform_finalize(GObject* object)
 }
 
 static void
-on_cog_headless_view_backend_destroy(CogHeadlessView *self)
-{
-    g_assert(self->exportable);
-    g_clear_pointer(&self->exportable, wpe_view_backend_exportable_fdo_destroy);
-}
-
-static WebKitWebViewBackend*
-cog_headless_platform_get_view_backend(CogPlatform* platform, WebKitWebView* related_view, GError** error)
-{
-    CogHeadlessPlatform *self = COG_HEADLESS_PLATFORM(platform);
-
-    struct wpe_view_backend *view_backend = wpe_view_backend_exportable_fdo_get_view_backend(self->view.exportable);
-    WebKitWebViewBackend    *wk_view_backend =
-        webkit_web_view_backend_new(view_backend, (GDestroyNotify) on_cog_headless_view_backend_destroy, &self->view);
-
-    g_assert(wk_view_backend);
-    return wk_view_backend;
-}
-
-static void
 cog_headless_platform_class_init(CogHeadlessPlatformClass* klass)
 {
     GObjectClass* object_class = G_OBJECT_CLASS(klass);
@@ -127,7 +145,7 @@ cog_headless_platform_class_init(CogHeadlessPlatformClass* klass)
 
     CogPlatformClass* platform_class = COG_PLATFORM_CLASS(klass);
     platform_class->setup = cog_headless_platform_setup;
-    platform_class->get_view_backend = cog_headless_platform_get_view_backend;
+    platform_class->get_view_type = cog_headless_view_get_type;
 }
 
 static void
@@ -146,6 +164,7 @@ g_io_cogplatform_headless_load(GIOModule* module)
 {
     GTypeModule* type_module = G_TYPE_MODULE(module);
     cog_headless_platform_register_type(type_module);
+    cog_headless_view_register_type(type_module);
 }
 
 G_MODULE_EXPORT void
