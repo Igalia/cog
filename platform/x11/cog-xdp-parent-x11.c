71e9f61a7921caa6a286e94fe23f5082b6a343e4
Joan Torres <joan.torres@suse.com>
2023-10-06 19:08:15 +0200

    x11: Add an XdpParent on file chooser dialog
    This adds a new implementation of XdpParent. To get the "parent_window"
    is simpler, it's just the window_id which is the window property of xcb.

---
+++ platform/x11/cog-xdp-parent-x11.c
@@ -0,0 +1,35 @@
+/*
+ * cog-xdp-parent-x11.c
+ * Copyright (C) 2023 SUSE Software Solutions Germany GmbH
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "cog-xdp-parent-x11.h"
+
+#include "../common/xdp-parent-private.h"
+
+static gboolean
+xdp_parent_export_x11(XdpParent *parent, XdpParentExported callback, gpointer user_data)
+{
+    xcb_window_t    *window_id = (xcb_window_t *) parent->data;
+    guint32          xid = (guint32) *window_id;
+    g_autofree char *handle = g_strdup_printf("x11:%x", xid);
+    callback(parent, handle, user_data);
+    return TRUE;
+}
+
+static void
+xdp_parent_unexport_x11(XdpParent *parent)
+{
+}
+
+XdpParent *
+xdp_parent_new_x11(xcb_window_t *window_id)
+{
+    XdpParent *parent = g_new0(XdpParent, 1);
+    parent->parent_export = xdp_parent_export_x11;
+    parent->parent_unexport = xdp_parent_unexport_x11;
+    parent->data = (gpointer) window_id;
+    return parent;
+}
---
+++ platform/x11/cog-xdp-parent-x11.h
@@ -0,0 +1,17 @@
+/*
+ * cog-xdp-parent-x11.h
+ * Copyright (C) 2023 SUSE Software Solutions Germany GmbH
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#pragma once
+
+#include <libportal/types.h>
+#include <xcb/xproto.h>
+
+G_BEGIN_DECLS
+
+XdpParent *xdp_parent_new_x11(xcb_window_t *window);
+
+G_END_DECLS
