bebbd91617b016b5a6a6e2a977c4ea1256144bd0
Pablo Saavedra <psaavedra@igalia.com>
2023-11-03 16:43:33 +0100

    wl: Fix unused variable display warning

--- platform/wayland/cog-platform-wl.c
+++ platform/wayland/cog-platform-wl.c
@@ -1343,7 +1343,6 @@ init_wayland(CogWlPlatform *platform, GError **error)
 static void
 clear_wayland(CogWlPlatform *platform)
 {
-    CogWlDisplay *display = platform->display;
     cog_wl_display_destroy(platform->display);
 }
 
